2c49dc28daddc7840cd747ce2237778151e555d9
risa06 <risamahardika89@gmail.com>
2020-11-21 22:50:56 +0700

    second commit

--- app/src/main/java/com/prakpapb1/booksapp/ItemData.java
+++ app/src/main/java/com/prakpapb1/booksapp/ItemData.java
@@ -1,8 +1,47 @@
 package com.prakpapb1.booksapp;
 
-public class ItemData {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class ItemData implements Parcelable {
     public String itemTitle;
     public String itemAuthor;
     public String itemImage;
     public String itemDescription;
+
+    public ItemData(){
+
+    }
+
+    protected ItemData(Parcel in) {
+        itemTitle = in.readString();
+        itemAuthor = in.readString();
+        itemImage = in.readString();
+        itemDescription = in.readString();
+    }
+
+    public static final Creator<ItemData> CREATOR = new Creator<ItemData>() {
+        @Override
+        public ItemData createFromParcel(Parcel in) {
+            return new ItemData(in);
+        }
+
+        @Override
+        public ItemData[] newArray(int size) {
+            return new ItemData[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(itemTitle);
+        parcel.writeString(itemAuthor);
+        parcel.writeString(itemImage);
+        parcel.writeString(itemDescription);
+    }
 }
